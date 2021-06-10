a4c47fadf2acfc6c938554b4e4b46949bfcafe9c
therealmartin <38257372+TheRealM4rtin@users.noreply.github.com>
2021-06-11 00:53:17 +0200

    Update but_celebration.c

--- but_celebration.c
+++ but_celebration.c
@@ -28,7 +28,7 @@ void lcd_but(){
 	char *lcd2 = "1 BUT DE PLUS !";
 	lcdClear(fd);
 	lcdPuts(fd,lcd1);
-	setCursor(uint8_t, 0, uint8_t, 1);
+	lcdCursor(u_int8_t, 0, u_int8_t, 1);
 	lcdPuts(fd,lcd2);
 }
 
