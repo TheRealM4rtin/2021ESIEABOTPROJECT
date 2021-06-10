2c9ffb4a0f8467a30688f3774fbf40aba453b6a9
Mario <dinamyx.ytb@gmail.com>
2021-06-10 12:06:43 +0200

    tourner

--- main.c
+++ main.c
@@ -53,6 +53,8 @@ int main (int argc, char ** argv){
     */
 	//On initialise l'ecran LCD puis on affiche un message
 	fd = lcdInit(2,16,4, rs,e,D4,D5,D6,D7,0,0,0,0);// initialise l'ecran LCD	
+	lcdClear(fd);			// permet d'effacer le contenu de l'ecran LCD
+	lcdPuts(fd,team); 
 	while (1)
     {  lcdClear(fd);			// permet d'effacer le contenu de l'ecran LCD
 		lcdPuts(fd,team); 	// on affiche le nom de l'Ã©quipe
