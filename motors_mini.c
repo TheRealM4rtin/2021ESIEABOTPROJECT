8603ae927acd995bba4533d6880f0a34da0e5a39
Mario <dinamyx.ytb@gmail.com>
2021-06-10 11:44:11 +0200

    tourner

--- motors_mini.c
+++ motors_mini.c
@@ -73,8 +73,8 @@ void moteurs(char BOUTONS[12])
         digitalWrite(enablePin2, 1);  //active moteur : GPIOC=HIGH
         digitalWrite(m1, 1);         //GPIOA=HIGH , marche avant moteur gauche
         digitalWrite(m2, 0);         //GPIOB=LOW
-        digitalWrite(m3, 0);
-        digitalWrite(m4, 1);         //marche arrière moteur droit
+        //digitalWrite(m3, 0);
+        //digitalWrite(m4, 1);         //marche arrière moteur droit
         
         //allumage de led droite
 		digitalWrite(led_d,1);
@@ -96,8 +96,8 @@ void moteurs(char BOUTONS[12])
         digitalWrite(enablePin, 1);
         digitalWrite(m1, 0);
         digitalWrite(m2, 1); //marche avant du moteur droit
-        digitalWrite(m3, 1); //marche arrière du moteur gauche
-        digitalWrite(m4, 0);
+        //digitalWrite(m3, 1); //marche arrière du moteur gauche
+        //digitalWrite(m4, 0);
 
         digitalWrite(led_g,1);
         
