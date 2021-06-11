c3d6774fc6a309bcc2abbd9241a979a89f670eb0
therealmartin <38257372+TheRealM4rtin@users.noreply.github.com>
2021-06-11 10:35:34 +0200

    Update but_celebration.c

--- but_celebration.c
+++ but_celebration.c
@@ -63,26 +63,26 @@ void aff_7seg_but(int nb_goal){
 }
 
 
-void celebration(int *nb_goal){
+void celebration(int nb_goal){
 	int buts = 3;
 	
-	if(*nb_goal == 1){
-		aff_7seg_but(*nb_goal);
+	if(nb_goal == 1){
+		aff_7seg_but(nb_goal);
 		lcd_but();
 	}
 	
 	
-	if(*nb_goal == 2){
-		aff_7seg_but(*nb_goal);
+	if(nb_goal == 2){
+		aff_7seg_but(nb_goal);
 		lcd_but();
 	}
-	if( *nb_goal == 3){
+	if( nb_goal == 3){
 		lcd_victoire();
-		*nb_goal = 0;
+		nb_goal = 0;
 	}
-	if(*nb_goal == 0)
+	if(nb_goal == 0)
 	{
-		aff_7seg_but(*nb_goal);
+		aff_7seg_but(nb_goal);
 	}
 	
 	
